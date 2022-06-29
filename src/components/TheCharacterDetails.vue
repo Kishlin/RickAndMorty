db3166ae657afa2a28e168af9ffcc2bf51a89c4e
Kishlin <pierre-louis.legrand@thetribe.io>
2022-06-29 18:47:18 +0200

    Refactor: Made a dedicated component for the character details

---
+++ src/components/TheCharacterDetails.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+import CharacterCardBig from "./characters/CharacterCardBig.vue";
+
+export default defineComponent({
+  components: {
+    CharacterCardBig,
+  },
+  props: ["character"],
+});
+</script>
+
+<template>
+  <section>
+    <div class="link">
+      <router-link to="/characters" custom v-slot="{ navigate }">
+        <button @click="navigate" @keypress.enter="navigate" role="link">
+          Back to characters
+        </button>
+      </router-link>
+    </div>
+    <p v-if="undefined === character">This character does not exist.</p>
+    <CharacterCardBig v-else :character="character" />
+  </section>
+</template>
+
+<style scoped>
+button[role="link"] {
+  padding: 5px 10px;
+
+  cursor: pointer;
+}
+
+.link {
+  text-align: right;
+  margin-bottom: 20px;
+}
+</style>
--- src/views/CharacterView.vue
+++ src/views/CharacterView.vue
@@ -3,11 +3,11 @@ import { defineComponent } from "vue";
 
 import { store } from "../store";
 
-import CharacterCardBig from "../components/characters/CharacterCardBig.vue";
+import TheCharacterDetails from "../components/TheCharacterDetails.vue";
 
 export default defineComponent({
   components: {
-    CharacterCardBig,
+    TheCharacterDetails,
   },
   computed: {
     /**
@@ -21,38 +21,5 @@ export default defineComponent({
 </script>
 
 <template>
-  <section>
-    <div class="link">
-      <router-link to="/characters" custom v-slot="{ navigate }">
-        <span @click="navigate" @keypress.enter="navigate" role="link"
-          >Back to characters</span
-        >
-      </router-link>
-    </div>
-    <p v-if="undefined === character">This character does not exist.</p>
-    <CharacterCardBig v-else :character="character" />
-  </section>
+  <TheCharacterDetails :character="character" />
 </template>
-
-<style scoped>
-span[role="link"] {
-  background-color: var(--color-heading);
-  border: var(--color-border) 3px solid;
-  border-radius: 5px;
-
-  color: var(--vt-c-indigo);
-
-  padding: 5px 10px;
-
-  cursor: pointer;
-}
-
-span[role="link"]:hover {
-  border-color: var(--color-border-hover);
-}
-
-.link {
-  text-align: right;
-  margin-bottom: 20px;
-}
-</style>
